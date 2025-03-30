ed5e5abfe7f370fb6f2b27c22a2f231c1820915a
muhammadirvann <muhammadirvan011206@gmail.com>
2025-03-31 06:18:23 +0700

    Displaying menus dynamically using JavaScript.

---
+++ assets/js/list-menu.js
@@ -0,0 +1,102 @@
+const menus = [
+  {
+    id: 1,
+    name: "Cheese Burger",
+    price: 24000,
+    category: "makanan",
+    image: "assets/images/burger.jpg",
+  },
+  {
+    id: 2,
+    name: "Chiken Teriyaki",
+    price: 25000,
+    category: "makanan",
+    image: "assets/images/chiken-teriyaki.png",
+  },
+  {
+    id: 3,
+    name: "Nasi Goreng",
+    price: 20000,
+    category: "makanan",
+    image: "assets/images/fried-rice.png",
+  },
+  {
+    id: 4,
+    name: "Popcorn",
+    price: 8000,
+    category: "snack",
+    image: "assets/images/popcorn.jpg",
+  },
+  {
+    id: 5,
+    name: "Granola Bars",
+    price: 5000,
+    category: "snack",
+    image: "assets/images/granola-bars.jpg",
+  },
+  {
+    id: 6,
+    name: "French Fries",
+    price: 10000,
+    category: "snack",
+    image: "assets/images/french-fries.png",
+  },
+  {
+    id: 7,
+    name: "Iced Caramel",
+    price: 12000,
+    category: "minuman",
+    image: "assets/images/iced-caramel.png",
+  },
+  {
+    id: 8,
+    name: "Matcha Milk",
+    price: 15000,
+    category: "minuman",
+    image: "assets/images/matcha_milk_tea.png",
+  },
+  {
+    id: 9,
+    name: "Iced Lemon Tea",
+    price: 7000,
+    category: "minuman",
+    image: "assets/images/iced-lemon-tea.jpg",
+  },
+];
+
+const menuContainer = document.getElementById("menu-container");
+
+function showMenus() {
+  menuContainer.innerHTML = "";
+
+  menus.forEach((menu) => {
+    const menuDiv = document.createElement("div");
+    menuDiv.classList.add("col-6", "col-lg-4", "menu-item");
+    menuDiv.setAttribute("data-category", menu.category);
+
+    menuDiv.innerHTML = `
+        <div class="card cursor-pointer">
+                <div class="card-body p-4">
+                    <img src="${menu.image}" alt="${
+      menu.name
+    }" class="w-75 d-block mx-auto"/>
+                    <h4 class="card-title mt-4 mb-2">${menu.name}</h4>
+                    <div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-1">
+                        <p class="mb-0 text-secondary fs-7">${menu.category}</p>
+                        <p class="mb-0 text-primary fw-semibold">Rp. ${menu.price.toLocaleString()}</p>
+                    </div>
+                </div>
+            </div>
+    `;
+
+    menuDiv.addEventListener("click", () => addToCart(menu));
+
+    menuContainer.appendChild(menuDiv);
+  });
+}
+
+showMenus();
+
+function addToCart(menu) {
+  console.log(`Menu ${menu.name} ditambahkan ke cart!`);
+}
--- assets/js/menu-categories.js
+++ assets/js/menu-categories.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
       this.classList.add("active");
 
-      const filter = this.getAttribute("data-filter");
+      const filter = this.dataset.filter;
 
       menuItems.forEach((item) => {
-        if (filter === "all" || item.getAttribute("data-category") === filter) {
+        const category = item.dataset.category;
+
+        if (filter === "all" || category === filter) {
           item.style.display = "flex";
         } else {
           item.style.display = "none";

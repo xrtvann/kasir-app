a5450952018db43ee9389054423673f914509d7b
muhammadirvann <muhammadirvan011206@gmail.com>
2025-04-04 17:55:09 +0700

    add register page

--- assets/js/show-hide-password.js
+++ assets/js/show-hide-password.js
@@ -2,3 +2,8 @@ document.getElementById("showPassword").addEventListener("change", function () {
   let passwordInput = document.getElementById("password");
   passwordInput.type = this.checked ? "text" : "password";
 });
+
+document.getElementById("showPassword").addEventListener("change", function () {
+    let passwordInput = document.getElementById("confirmpassword");
+    passwordInput.type = this.checked ? "text" : "password";
+  });
